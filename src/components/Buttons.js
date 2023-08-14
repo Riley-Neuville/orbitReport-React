7f3b8389e27f5d93946d6a6b92f6f9314b6648c3
Riley Neuville <Riley.Neuville@gmail.com>
2023-08-14 00:55:19 -0500

    Completed Project

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,24 @@
-const Buttons = () => {
+import satData from "./satData.js";
+import React from 'react';
+
+function Buttons({ filterByType, setSat, displaySats }) {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-      </div>
+      {displaySats.map((sat, key) => (
+        <button
+          key={sat.id}
+          onClick={() => filterByType(sat)}
+        >
+          {sat} orbit
+        </button>
+      ))}
+      <button
+        onClick={() => setSat(satData)}
+      >
+        All Orbit
+      </button>
+    </div>
   );
-};
+}
 
 export default Buttons;
